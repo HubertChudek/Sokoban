2b893cf43e17ede60125720bb981528a7bc6fd59
Hubert Chudek <hubert.chudek@gmail.com>
2020-05-14 20:22:18 +0200

    encapsulate button initiation code

--- src/Sokoban.java
+++ src/Sokoban.java
@@ -15,7 +15,16 @@ public class Sokoban extends JFrame {
         Board board = new Board();
         add(board);
         board.setLayout(null);
+        initMenuBttn(board);
 
+        setTitle("Sokoban");
+        setSize(board.getBoardWidth() + OFFSET, board.getBoardHeight() + 2 * OFFSET);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //ustawia akcję na kliknięcie przycisku zamykania(nie jest ustawiona domyślnie)
+        setLocationRelativeTo(null);                        //ustawia pozycję okna w odniesieniu do innego komponentu, tu wyśrodkowane
+        setResizable(false);
+    }
+
+    public void initMenuBttn(Board board) {
         var menuButton = new JButton(new ImageIcon("assets/pause.png"));
         menuButton.setMargin(new Insets(0, 0, 0, 0));
         menuButton.addActionListener((event) -> board.togglePause());
@@ -23,12 +32,6 @@ public class Sokoban extends JFrame {
         menuButton.setFocusable(false);
         menuButton.setBounds(board.getBoardWidth() - 2 * OFFSET, OFFSET, 40, 40);
         board.add(menuButton);
-
-        setTitle("Sokoban");
-        setSize(board.getBoardWidth() + OFFSET, board.getBoardHeight() + 2 * OFFSET);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //ustawia akcję na kliknięcie przycisku zamykania(nie jest ustawiona domyślnie)
-        setLocationRelativeTo(null);                        //ustawia pozycję okna w odniesieniu do innego komponentu, tu wyśrodkowane
-        setResizable(false);
     }
 
     public static void main(String[] args) {
