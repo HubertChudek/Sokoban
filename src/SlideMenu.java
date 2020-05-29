c630946ce1d332ca541c68c44e63d95cc5255260
Hubert Chudek <hubert.chudek@gmail.com>
2020-05-29 19:56:56 +0200

    create SplitPAnel class and rename SplitMenu class to SlideMenu

---
+++ src/SlideMenu.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class SlideMenu extends JPanel {
+
+    public SlideMenu() {
+
+    }
+}
---
+++ src/SplitPanel.java
@@ -0,0 +1,5 @@
+public class SplitPanel {
+
+    public SplitPanel() {
+    }
+}
